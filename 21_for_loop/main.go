cb725eb4aa5f43edc9389e6ad6bc049d4b88353a
bayram bartu <bayram213062@ogr.duzce.edu.tr>
2024-09-01 00:11:51 +0300

    Create main.go

---
+++ 21_for_loop/main.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+	// for i := 0; i <= 10; i += 5 {
+	// 	fmt.Println(i)
+	// }
+	// for <init statement>; condition; <post statement>
+
+	/* i := 0
+	for ; i <= 10; i += 5 {
+		fmt.Println(i)
+	}
+	fmt.Println(i) */
+
+	/* for { // Infinite loop
+		fmt.Println("bartu")
+	}*/
+	// for i := 0; false; i += 5 {
+	// 	fmt.Println(i)
+	// }
+	/*i := 10
+
+	for i >= 0 {
+		fmt.Println(i)
+		i--
+	}*/
+	/* for i := 0; i <= 10; i++ { // continue --> döngünün başına git
+		if i%3 == 0 {
+			continue
+		}
+		if i == 5 { // break--> döngüden çık
+			break
+		}
+		fmt.Println(i)
+	} */
+}
