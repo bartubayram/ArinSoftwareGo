6f4b31252d137ac5865b7ed6d9a7bfaabb04e528
Bayram Bartu <113308882+bayrambartu@users.noreply.github.com>
2024-09-09 00:09:24 +0300

    slicepart_II
    slices

--- main.go
+++ main.go
@@ -3,72 +3,69 @@ package main
 import "fmt"
 
 func main() {
-	// aynı veri tipine ait birden fazla değişken varsa liste içerisinde tutarız bu liste yapısına array denir
-	/* 	city1 := "istanbul"
-	   	city2 := "roma"
+	/* underArray := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(underArray)
 
-	   	city3 := "tahran"
-	   	city4 := "belgrad"
-	   	fmt.Println(city1, city2, city3, city4) */
-	// cities := [4]string{"istanbul", "roma", "tahran", "belgrad"}
+	mySlc := underArray[2:5] // -> underArray'deki indeks numarlarını(aralık olarak-> 2-3-4. indekleri yazdırır.) göstermektedir.
+	fmt.Println(mySlc)
+	mySlc2 := underArray[:6]
+	fmt.Println(mySlc2)
+	mySlc3 := underArray[3:]
+	fmt.Println(mySlc3)
+	mySlc4 := underArray[:]
+	fmt.Println(mySlc4)
 
-	/*
-		 	cities := [...]string{"istanbul", "roma", "tahran", "belgrad"}
+	fmt.Println(mySlc)
+	mySlc[0] = 100
+	fmt.Println(mySlc) */
 
-			fmt.Println(cities)
-			fmt.Println(cities[0]) // zero base index
-			fmt.Println(cities[3])
-			fmt.Println(len(cities))
+	/* 	mySlc := []int{1, 2, 3} // mySliceUnderArray
+	   	fmt.Println(mySlc) */
+
+	/* 	mySlc = append(mySlc, 4, 5)
+	   	fmt.Println(mySlc) */
+
+	/* mySlc2 := append(mySlc, 4, 5, 6) // mySlice2UnderArray -> aynı underarray üzerinden işlem yapmıyor çünkü ekleme yapınca aynı array'e ait olmus olmuyor(array'lere ekleme çıkarma yapılamaz.)
+	fmt.Println(mySlc2)
+	mySlc[0] = 100
+	fmt.Println(mySlc)
+	fmt.Println(mySlc2) */
+
+	/* mySlc := []int{1, 2, 3}
+	mySlc2 := []int{4, 5}
+	fmt.Println(mySlc)
+	mySlc = append(mySlc, mySlc2...)
+	fmt.Println(mySlc) */
+
+	// mySlc := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// mySlc = mySlc[2:] // ilk n elamanı silme için [n:]
+	/* 	fmt.Println(mySlc)
+	 */
+	/* mySlc = mySlc[:len(mySlc)-3] // son n elemanı silme için [:len(mySlc)-(n)]
+	fmt.Println(mySlc) */
+
+	/* mySlc = mySlc[2:]
+	mySlc = mySlc[:len(mySlc)-3]
+	fmt.Println(mySlc)
 	*/
-	// başlangıç değeri kendinden olan array nasıl oluştururuz?
-	/* var myArray [5]int
-	fmt.Println(myArray)
-	myArray[0] = 13
-	fmt.Println(myArray)
-	myArray[len(myArray)-1] = 200
-	fmt.Println(myArray) */
 
 	/* 	var myArr [4]int
-	   	var myArr2 [5]int */
+	   	fmt.Println(myArr)
 
-	/* 	if myArr == myArr2 {
-		fmt.Println("message")
-	} */
-	// fmt.Println(myArr, "\n", myArr2)
+	   	var mySlc []int
+	   	fmt.Println(mySlc)
 
-	/* cities := [4]string{"istanbul", "roma", "tahran", "belgrad"}
-	for i := 0; i < len(cities); i++ {
-		fmt.Println(i, cities[i])
-	}
-	cities[0] = "Ankara"
-	fmt.Println()
-	for i := 0; i < len(cities); i++ {
-		fmt.Println(i, cities[i])
-	} */
-	/*
-		myArr := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	   	var mySlc2 []int
+	   	mySlc2 = make([]int, 4) // Zero değerler Slice Elemanlarına ait olan
+	   	fmt.Println(mySlc2)
 
-		// Tekrardan aynı array'e atacağız.
-		myArr = mySquare(myArr) // First Class Functions
-		fmt.Println(myArr) */
+	   	var mySlc3 []bool
+	   	mySlc3 = make([]bool, 2) // Zero değerler Slice Elemanlarına ait olan
+	   	fmt.Println(mySlc3) */
 
-	// FOR- RANGE
-	// for  index, value := range myArr{}
-	cities := [4]string{"istanbul", "roma", "tahran", "belgrad"}
-	/* 	for index, city := range cities { // -> range yapsıı burda dizimizin son elemanına kadar olan elemanları yazdırmamızı sağlar
-
-		fmt.Println(index, city)
-		fmt.Println(city) ---> Hata Alırız
-	} */
-	for _, city := range cities { //Blank Identifier ..!
-		fmt.Println(city)
-	}
-
+	var mySlc4 []int
+	fmt.Printf("%#v", mySlc4) // declare ettik ama oluşturmadık yani bu slice yok
+	fmt.Println()             // for line
+	mySlc5 := make([]int, 0)  // declare ettik ve oluşturduk bu slice var ama elemanı yok yani boş
+	fmt.Printf("%#v", mySlc5)
 }
-
-/* func mySquare(arr [10]int) [10]int {
-for i := 0; i < len(arr); i++ {
-	arr[i] = arr[i] * arr[i]
-}
-return arr
-}*/
